445d48e995e987e24bd33d80137ca5baedfcd6d7
Diego Ximendes <62144127+diegoximd@users.noreply.github.com>
2021-07-19 23:03:42 -0300

    Add files via upload

---
+++ src/main/java/one/digitalinnovation/experts/produtctcatalog/ProdutctCatalogApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.experts.produtctcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutctCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutctCatalogApplication.class, args);
+	}
+
+}
---
+++ src/main/java/one/digitalinnovation/experts/produtctcatalog/config/ElasticSearchConfig.java
@@ -0,0 +1,35 @@
+package one.digitalinnovation.experts.produtctcatalog.config;
+
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.support.DefaultConversionService;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.RestClients;
+import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+import javax.swing.text.html.parser.Entity;
+
+@Configuration
+@EnableElasticsearchRepositories(basePackages = "one.digitalinnovation.experts.productcatalog.repository")
+public class ElasticSearchConfig extends AbstractElasticsearchConfiguration {
+
+    @Override
+    public RestHighLevelClient elastisearchCliente() {
+        ClientConfiguration clientConfiguration = ClientConfiguration.builder()
+                .connectedTo("localhost:9200", "localhost:9300")
+                .build();
+        return RestClients.create(clientConfiguration).rest();
+    }
+
+    @Bean
+    @Override
+    public EntityMapper entityMapper() {
+        ElasticsearchEntityMapper entityMapper = new ElasticsearchEntityMapper(elastsearchEntityMappingContext(),
+            new DefaultConversionService());
+        entityMapper.setConversions(elasticsearchCustomConversions());
+        return entityMapper;
+    }
+
+}
